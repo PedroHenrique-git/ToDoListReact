661e7fad011f4ee4b375e33e6624193aead4ec0a
pedro <pedromf34@outlook.com>
2020-11-05 22:55:19 -0300

    Organizando arquivos

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,35 +1,5 @@
 import React from 'react';
-
-function ButtonEditar(props){
-    return (
-        <button onClick={props.onClick} className="editar">{props.value}</button>
-    );
-}
-function ButtonExcluir(props){
-    return (
-        <button onClick={props.onClick} className="excluir">{props.value}</button>
-    );
-}
-
-class ListItem extends React.Component{
-    render(){
-        return(
-            <ul>
-                {this.props.tks.map((task,index) => (
-                <li key={index}>
-                    <h1>{task.nameTask}</h1>
-                    <p>{task.descTask}</p>
-                    <div className="div-buttons">
-                        <ButtonEditar onClick={() => this.props.update(index)} value={this.props.text} className="editar"/>
-                        <ButtonExcluir onClick={() => this.props.delete(index)}  value="Exluir" className="exluir"/>
-                    </div>
-                </li>
-                ))}
-            </ul>
-        ); 
-    }
-        
-}
+import ListItem from './ListItem';
 
 export default class TodoApp extends React.Component{
     constructor(props){
