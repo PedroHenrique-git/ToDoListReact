cf2d09dd4859cd6a266cbf218e74d8651ba835bf
PedroHenrique-git <pedromf34@outlook.com>
2021-07-11 17:18:28 -0300

    mudanças

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import { ToastContainer } from 'react-toastify';
 import App from './App';
 import './style/index.scss';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
   <StrictMode>
+    <ToastContainer />
     <App />
   </StrictMode>,
   document.getElementById('root'),
--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -1,8 +1,9 @@
 import './index.scss';
-import { AiOutlineSearch } from 'react-icons/ai';
+import { RiAddCircleFill } from 'react-icons/ri';
 import { FaRegEdit } from 'react-icons/fa';
 import { BsFillTrashFill } from 'react-icons/bs';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import { toast } from 'react-toastify';
 
 export default function Index() {
   const [task, setTask] = useState('');
@@ -11,8 +12,32 @@ export default function Index() {
   const nameTask = useRef(null);
   const inputEdit = useRef(null);
 
+  useEffect(() => {
+    const savedTasks = localStorage.getItem('tasks');
+
+    if (savedTasks) {
+      setTasks(JSON.parse(savedTasks));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  const checkIfExistsTask = (item) => {
+    const index = tasks.findIndex((value) => value === item);
+    if (index !== -1) return true;
+    return false;
+  };
+
   const addTask = (item) => {
-    setTasks([...tasks, item]);
+    if (!checkIfExistsTask(item) && item) {
+      setTasks([...tasks, item]);
+      setTask('');
+      toast.success('task created successfully');
+    } else {
+      toast.error('task already exists or invalid value');
+    }
   };
 
   const removeTask = (index) => {
@@ -28,11 +53,21 @@ export default function Index() {
 
   const handleFocusOutEdit = (index) => {
     const copyState = [...tasks];
-    if (!taskEdit) return;
+
+    if (!taskEdit || checkIfExistsTask(taskEdit)) {
+      toast.error('task already exists or invalid value');
+
+      nameTask.current.style.display = 'block';
+      inputEdit.current.style.display = 'none';
+
+      return;
+    }
+
     copyState.splice(index, 1, taskEdit);
     setTasks(copyState);
     nameTask.current.style.display = 'block';
     inputEdit.current.style.display = 'none';
+    toast.success('task edited successfully');
   };
 
   const handleSubmit = (e) => {
@@ -46,9 +81,15 @@ export default function Index() {
         <div className="task_section-wrap">
           <div className="task_form">
             <form onSubmit={(e) => handleSubmit(e)}>
-              <input type="text" onChange={(e) => setTask(e.target.value)} />
+              <input
+                type="text"
+                placeholder="add task"
+                onChange={(e) => setTask(e.target.value)}
+                value={task}
+                autoComplete="off"
+              />
               <button type="submit">
-                <AiOutlineSearch size={42} color="#d1e5e7" />
+                <RiAddCircleFill size={30} />
               </button>
             </form>
           </div>
@@ -59,8 +100,10 @@ export default function Index() {
                   ref={inputEdit}
                   className="edit_input"
                   type="text"
+                  placeholder="edit task"
                   onBlur={() => handleFocusOutEdit(index)}
                   onChange={(e) => setTaskEdit(e.target.value)}
+                  autoComplete="off"
                 />
                 <p ref={nameTask}>{value}</p>
                 <div className="task_actions">
